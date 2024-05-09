bbc19ba47aa5ac612ca4571aee5f28b9da946df9
Chaitanya <boby2965@gmail.com>
2024-05-09 14:36:32 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,41 @@
+
+console.log("******************")
+console.log("TASK 1A")
+for (var a=1; a<=10; a++){
+    console.log( `2 * ${a} = ${2*a}`);
+}
+console.log("******************")
+
+
+console.log("TASK 1B")
+for (var a=1; a<=10; a++){
+    console.log( `3 * ${a} = ${3*a}`);
+}
+console.log("******************")
+
+
+console.log("TASK 2")
+for(a=0; a<11; a+=2){
+    console.log(a);
+}
+console.log("******************")
+
+
+console.log("TASK 3")
+for(a=10; a>=0; a-=2){
+    console.log(a);
+}
+console.log("******************")
+
+
+console.log("TASK 4")
+for(a=1; a<=10; a++){
+    console.log(a);
+}
+console.log("******************")
+
+console.log("TASK 5")
+for(a=10; a>=0; a--){
+    console.log(a);
+}
+console.log("******************")
